2d3976e2ac1d247764f298511b8b83afdcf5e5fd
c_plus_c <c.plus.c.haltman@gmail.com>
2013-12-04 14:57:51 +0900

    飛行機が動くようになった。

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@
 LIB3D = 1
 
 # ソースファイル
-C_SRC = main.c export.c pad.c fireball.c player.c
+C_SRC = main.c export.c pad.c fireball.c player.c pad.c
 
 # CGROM ディレクトリ
 CGROMDIR = ./
--- main.c
+++ main.c
@@ -116,41 +116,6 @@ void draw( int frame , int motion_number  ) {
 };
 
 	player_drw(NULL);
-	/* ツリー→ワールド座標変換 */
-	//ag3dSetRoot( 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, node );
-
-	/* ツリー形状を求める */
-	//ag3dCalcTree( &(age3dTree[ AG_AG3D_AG3DEXPORTTREE ]), &(age3dMotion[ 0 ]), (float)frame, node );
-
-	/* 描画 */
-		/* 不透明 */
-	//agglDisable( AGGL_BLEND );
-	//agglDepthMask( AGGL_TRUE );
-
-	//ag3dDrawAnimenodeDCmd( &(age3dModel[ AG_AG3D_AG3DEXPORTMODEL ]), node, &(age3dDCmd[ AG_AG3D_AG3DEXPORTMODEL ]), AG3D_OFFBLEND_ONDEPTH );
-
-		/* 半透明、Ｚバッファ更新 */
-	//agglEnable( AGGL_BLEND );
-
-	//agglBeginZsort( AGGL_FAR_FIRST, sizeof(zsortbuf), zsortbuf );
-	//ag3dDrawAnimenode( &(age3dModel[ AG_AG3D_AG3DEXPORTMODEL ]), node, AG3D_ONBLEND_ONDEPTH );
-	//agglEndZsort();
-
-		/* 半透明、Ｚバッファ非更新 */
-	//agglEnable( AGGL_BLEND );
-	//agglDepthMask( AGGL_FALSE );
-
-	//agglBeginZsort( AGGL_FAR_FIRST, sizeof(zsortbuf), zsortbuf );
-	//ag3dDrawAnimenode( &(age3dModel[ AG_AG3D_AG3DEXPORTMODEL ]), node, AG3D_ONBLEND_OFFDEPTH );
-	//agglEndZsort();
-
-	//agglDepthMask( AGGL_TRUE );
-
-	/* フレーム描画終了 */
-	//err = agglGetError();
-	//if ( err != 0 ) {
-	//	_dprintf( "!!!!!!!!!!!!!! err = %d, frame = %d !!!!!!!!!!!\n" , err, frame );
-	//}
 
 }
 
