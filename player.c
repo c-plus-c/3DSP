2d8b944d0b0a282fc8bd07569ab0c5abedb870bd
c_plus_c <c.plus.c.haltman@gmail.com>
2013-12-20 16:53:12 +0900

    レンダリングの高速化を試みる

--- player.c
+++ player.c
@@ -9,7 +9,7 @@ void player_init()
 {
 	playerObject.Obj.translation.X=0;
 	playerObject.Obj.translation.Y=0;
-	playerObject.Obj.translation.Z=50;
+	playerObject.Obj.translation.Z=30;
 	
 	playerObject.Obj.direction.X=0;
 	playerObject.Obj.direction.Y=0;
@@ -54,23 +54,23 @@ void player_move(Object *dp)
   if( PadLvl() & PAD_RIGHT )
     {
       //roll=-0.3;
-      playerObject.Obj.yaw+=0.05;
+      playerObject.Obj.yaw-=0.04;
     }
   
   if( PadLvl() & PAD_LEFT )
     {
       //roll=0.3;
-      playerObject.Obj.yaw-=0.05;
+      playerObject.Obj.yaw+=0.04;
     }
 
   if (PadLvl() & PAD_DOWN)
     {
-      playerObject.Obj.pitch+=0.05;
+      playerObject.Obj.pitch+=0.04;
     }
 
   if(PadLvl() & PAD_UP)
     {
-      playerObject.Obj.pitch-=0.05;
+      playerObject.Obj.pitch-=0.04;
     }
 
   ch=cosf(playerObject.Obj.yaw);
@@ -82,16 +82,20 @@ void player_move(Object *dp)
 
   tx=0;
   ty=0;
-  tz=-1;
+  tz=-0.3;
 
-  playerObject.Obj.direction.X=(ch*cb+sh*sp*sb)*tx+(-ch*sb+sh*sp*cb)*ty+(sh*cp)*tz;
-  playerObject.Obj.direction.Y=(sb*cp)*tx+(cb*cp)*ty+(-sp)*tz;
-  playerObject.Obj.direction.Z=(-sh*cb+ch*sp*sb)*tx+(sb*sh+ch*sp*cb)*ty+(ch*cp)*tz;
+  //playerObject.Obj.direction.X=(ch*cb+sh*sp*sb)*tx+(-ch*sb+sh*sp*cb)*ty+(sh*cp)*tz;
+  //playerObject.Obj.direction.Y=(sb*cp)*tx+(cb*cp)*ty+(-sp)*tz;
+  //playerObject.Obj.direction.Z=(-sh*cb+ch*sp*sb)*tx+(sb*sh+ch*sp*cb)*ty+(ch*cp)*tz;
+  
+  playerObject.Obj.direction.X=(sh*cp)*tz;
+  playerObject.Obj.direction.Y=-sp*tz;
+  playerObject.Obj.direction.Z=(ch*cp)*tz;
   
   
-  playerObject.Obj.translation.X+=playerObject.Obj.direction.X*0.2;
-  playerObject.Obj.translation.Y+=playerObject.Obj.direction.Y*0.2; 
-  playerObject.Obj.translation.Z+=playerObject.Obj.direction.Z*0.2;
+  playerObject.Obj.translation.X+=playerObject.Obj.direction.X;
+  playerObject.Obj.translation.Y+=playerObject.Obj.direction.Y; 
+  playerObject.Obj.translation.Z+=playerObject.Obj.direction.Z;
 }
 
 /* TODO:今西
@@ -101,23 +105,17 @@ void player_drw(Object *dp)
 {
 
 	int err;
-	static float counter=0;
-
-	_dprintf("%f %f %f\n",playerObject.Obj.direction.X,playerObject.Obj.direction.Y,playerObject.Obj.direction.Z);
 	/* 描画 */
 		/* 不透明 */
 	agglDisable( AGGL_BLEND );
 	agglDepthMask( AGGL_TRUE );
 	
 	agglPushMatrix();
-	agglTranslatef(playerObject.Obj.translation.X,playerObject.Obj.translation.Y,playerObject.Obj.translation.Z);
-	counter+=0.05;
-
 	
-
-	agglRotatef(playerObject.Obj.roll*180/PI,0,0,1);
-	agglRotatef(playerObject.Obj.yaw*180/PI,0,1,0);
-	agglRotatef(playerObject.Obj.pitch*180/PI,1,0,0);
+	agglTranslatef(playerObject.Obj.translation.X,playerObject.Obj.translation.Y,playerObject.Obj.translation.Z);
+	agglRotatef(playerObject.Obj.roll/PI*180,0,0,1);
+	agglRotatef(playerObject.Obj.yaw/PI*180,0,1,0);
+	agglRotatef(playerObject.Obj.pitch/PI*180,1,0,0);
 	
 		/* ツリー→ワールド座標変換 */
 	ag3dSetRoot( 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, node );
@@ -134,14 +132,14 @@ void player_drw(Object *dp)
 	agglBeginZsort( AGGL_FAR_FIRST, sizeof(zsortbuf), zsortbuf );
 	ag3dDrawAnimenode( &(age3dModel[ AG_AG3D_AG3DEXPORTMODEL ]), node, AG3D_ONBLEND_ONDEPTH );
 	agglEndZsort();
-
+       
 		/* 半透明、Ｚバッファ非更新 */
-	agglEnable( AGGL_BLEND );
-	agglDepthMask( AGGL_FALSE );
+	//agglEnable( AGGL_BLEND );
+	//agglDepthMask( AGGL_FALSE );
 
-	agglBeginZsort( AGGL_FAR_FIRST, sizeof(zsortbuf), zsortbuf );
-	ag3dDrawAnimenode( &(age3dModel[ AG_AG3D_AG3DEXPORTMODEL ]), node, AG3D_ONBLEND_OFFDEPTH );
-	agglEndZsort();
+	//agglBeginZsort( AGGL_FAR_FIRST, sizeof(zsortbuf), zsortbuf );
+	//ag3dDrawAnimenode( &(age3dModel[ AG_AG3D_AG3DEXPORTMODEL ]), node, AG3D_ONBLEND_OFFDEPTH );
+	//agglEndZsort();
 
 	agglDepthMask( AGGL_TRUE );
 
