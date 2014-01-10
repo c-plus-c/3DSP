ee02db2c94303fec770531236a7b11bd2752af7e
c_plus_c <c.plus.c.haltman@gmail.com>
2014-01-10 17:13:00 +0900

    一応早くなった

--- plane.c
+++ plane.c
@@ -4,6 +4,8 @@
 #include "extern.h"
 void DrawPlane()
 {
+	agglEnable( AGGL_BLEND );
+	agglDepthMask( AGGL_TRUE );
 	
 	agglPushMatrix();
 	agglScalef(5000,1,5000);
@@ -12,7 +14,7 @@ void DrawPlane()
 
 		/* ツリー形状を求める */
 	ag3dCalcTree( &(age3dTree[ AG_AG3D_PLANETREE ]), &(age3dMotion[ AG_AG3D_PLANEMOTION ]), (float)0, node );
-	ag3dDrawAnimenodeDCmd( &(age3dModel[ AG_AG3D_PLANEMODEL ]), node,&(age3dDCmd[ AG_AG3D_PLANEMODEL ]), AG3D_ONBLEND_ONDEPTH  );
+	ag3dDrawAnimenodeDCmd( &(age3dModel[ AG_AG3D_PLANEMODEL ]), node,&(age3dDCmd[ AG_AG3D_PLANEMODEL ]), AG3D_ONBLEND_ONDEPTH);
 
 	
 		/* 半透明、Ｚバッファ更新 */
