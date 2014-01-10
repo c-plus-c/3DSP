c6028c12575978c4bb0de3f34ce5dafeb44e9d31
c_plus_c <c.plus.c.haltman@gmail.com>
2014-01-10 16:29:47 +0900

    高速化プロトタイプ(失敗)

--- backsky.c
+++ backsky.c
@@ -12,24 +12,24 @@ void DrawSky()
 
 		/* ツリー形状を求める */
 	ag3dCalcTree( &(age3dTree[ AG_AG3D_SKYBACKTREE ]), &(age3dMotion[ AG_AG3D_SKYBACKMOTION ]), (float)1, node );
-	ag3dDrawAnimenode( &(age3dModel[ AG_AG3D_SKYBACKMODEL ]), node, AG3D_OFFBLEND_ONDEPTH );
+	ag3dDrawAnimenodeDCmd( &(age3dModel[ AG_AG3D_SKYBACKMODEL ]), node,&(age3dDCmd[ AG_AG3D_SKYBACKMODEL ]), AG3D_ONBLEND_ONDEPTH );
 	
 		/* 半透明、Ｚバッファ更新 */
-	agglEnable( AGGL_BLEND );
+	//agglEnable( AGGL_BLEND );
 
-	agglBeginZsort( AGGL_FAR_FIRST, sizeof(zsortbuf), zsortbuf );
-	ag3dDrawAnimenode( &(age3dModel[ AG_AG3D_SKYBACKMODEL ]), node, AG3D_ONBLEND_ONDEPTH );
-	agglEndZsort();
+	//agglBeginZsort( AGGL_FAR_FIRST, sizeof(zsortbuf), zsortbuf );
+	//ag3dDrawAnimenode( &(age3dModel[ AG_AG3D_SKYBACKMODEL ]), node, AG3D_ONBLEND_ONDEPTH );
+	//agglEndZsort();
 
 		/* 半透明、Ｚバッファ非更新 */
-	agglEnable( AGGL_BLEND );
-	agglDepthMask( AGGL_FALSE );
+	//agglEnable( AGGL_BLEND );
+	//agglDepthMask( AGGL_FALSE );
 
-	agglBeginZsort( AGGL_FAR_FIRST, sizeof(zsortbuf), zsortbuf );
-	ag3dDrawAnimenode( &(age3dModel[ AG_AG3D_SKYBACKMODEL ]), node, AG3D_ONBLEND_OFFDEPTH );
-	agglEndZsort();
+	//agglBeginZsort( AGGL_FAR_FIRST, sizeof(zsortbuf), zsortbuf );
+	//ag3dDrawAnimenode( &(age3dModel[ AG_AG3D_SKYBACKMODEL ]), node, AG3D_ONBLEND_OFFDEPTH );
+	//agglEndZsort();
 
-	agglDepthMask( AGGL_TRUE );
+	//agglDepthMask( AGGL_TRUE );
 	
 	agglPopMatrix();
 }
