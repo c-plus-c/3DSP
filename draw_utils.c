07aaeb917e2931df9eae50a94fd83905caaa013b
c_plus_c <c.plus.c.haltman@gmail.com>
2014-01-25 19:50:08 +0900

    死んだ自機をレーダーから表示させないようにした

--- draw_utils.c
+++ draw_utils.c
@@ -164,7 +164,7 @@ void drawRadar(Object *dp){
 	for(i=0;i<playerNum;i++){
 		Object *dp2 = &Objects[i];
 		int dx, dz;
-
+		if(dp2->stat==DEAD) continue;
 		drawTex(texs[dp2->pid],(int)(dp2->translation.X*100/ACTIVE_RADIUS+offsetX),(int)(dp2->translation.Z*100/ACTIVE_RADIUS+offsetY),
 			6,9,atan2f(dp2->direction.Z,dp2->direction.X)-PI/2);
 		drawNum((((int)(dp2->translation.X*100/ACTIVE_RADIUS)<<2) +(offsetX<<2)),
